d862502ebcdc2f9e8cca123fbeb193d0b7995045
pcentric <priyanshu.s1619@gmail.com>
2020-01-10 01:54:04 +0530

    ADD META CLASS

--- codedaddies_list/settings.py
+++ codedaddies_list/settings.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/1.11/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
-import django_heroku
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -122,4 +122,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
 django_heroku.settings(locals())
