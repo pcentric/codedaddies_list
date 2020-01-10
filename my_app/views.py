8a7c1ed2a4d4c2fa78753807f4ed6ca811361425
pcentric <priyanshu.s1619@gmail.com>
2020-01-11 04:36:14 +0530

    Intial Commit

--- my_app/views.py
+++ my_app/views.py
@@ -1,13 +1,11 @@
-# -*- coding: utf-8 -*-
 import requests
 from bs4 import BeautifulSoup
 from django.shortcuts import render
 from requests.compat import quote_plus
 from . import models
 
-BASE_CRAIGSLIST_URL = 'https://lucknow.craigslist.org/search/?query={}'
+BASE_CRAIGSLIST_URL = 'https://losangeles.craigslist.org/search/?query={}'
 BASE_IMAGE_URL = 'https://images.craigslist.org/{}_300x300.jpg'
-# Create your views here.
 
 
 def home(request):
@@ -29,10 +27,12 @@ def new_search(request):
     for post in post_listings:
         post_title = post.find(class_='result-title').text
         post_url = post.find('a').get('href')
-        if post.find(class_='result.price'):
+
+        if post.find(class_='result-price'):
             post_price = post.find(class_='result-price').text
         else:
             post_price = 'N/A'
+
         if post.find(class_='result-image').get('data-ids'):
             post_image_id = post.find(class_='result-image').get('data-ids').split(',')[0].split(':')[1]
             post_image_url = BASE_IMAGE_URL.format(post_image_id)
@@ -40,10 +40,11 @@ def new_search(request):
         else:
             post_image_url = 'https://craigslist.org/images/peace.jpg'
 
-        final_postings.append((post_title, post_url, post_price ,post_image_url))
+        final_postings.append((post_title, post_url, post_price, post_image_url))
 
-    stuff_frontend = {
+    stuff_for_frontend = {
         'search': search,
         'final_postings': final_postings,
     }
-    return render(request, 'my_app/new_search.html', stuff_frontend)
+
+    return render(request, 'my_app/new_search.html', stuff_for_frontend)
