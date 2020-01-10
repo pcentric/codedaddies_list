a9bf719e914d0832ec8faf631ee6c8cfe12f2f34
pcentric <priyanshu.s1619@gmail.com>
2020-01-11 05:13:59 +0530

    Intial Commit

--- my_app/urls.py
+++ my_app/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import  url
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-    url(r'^new_search', views.new_search, name='new_search'),
+    url(r'^new_search/', views.new_search, name='new_search'),
 
 ]
